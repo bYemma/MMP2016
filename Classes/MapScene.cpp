8eec20cb536e6f061bd087055c941082284d8adb
Kilian <kontakt@byemma.de>
2016-06-30 10:43:29 +0200

    Bisschen herumgespielt an meiner MapScene

--- Classes/MapScene.cpp
+++ Classes/MapScene.cpp
@@ -1,4 +1,5 @@
 #include "MapScene.h"
+#include "GameMap.h"
 #include "GameLayer.h"
 #include "iostream"
 
@@ -21,6 +22,24 @@ bool MapScene::init()
 	}
 
 	_sSize = Director::getInstance()->getWinSize();
+	int height = _sSize.height;
+	int width = _sSize.width;
 
+	GameMap map;
+	map.createMap(0.5, 0.1, 0.1, 100);
+	vector<double> points = map.getPoints();
+	/*
+	auto rectNode = DrawNode::create();
+	Vec2 map_shape[102];
+	map_shape[0] = Vec2(0, 0);
+	for (unsigned i = 0; i < points.size(); ++i) {
+		map_shape[i+1] = Vec2(width / 100 * i, height * points[i]);
+	}
+	map_shape[101] = Vec2(width, 0);
+
+	Color4F white(1, 1, 1, 1);
+	rectNode->drawPolygon(map_shape, 102, white, 1, white);
+	this->addChild(rectNode);
+	*/
 	return true;
 }
