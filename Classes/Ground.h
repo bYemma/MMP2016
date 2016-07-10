33fec4e36049861ed1e24f21d4cc7b8ed9c0f06b
HernanGonzalez42 <hernanhtml@hotmail.es>
2016-07-10 21:50:50 +0200

    Manually commit my changes, 2/4

--- Classes/Ground.h
+++ Classes/Ground.h
@@ -5,36 +5,52 @@
 #include <cstdlib>
 #include <ctime>
 
-#include "Projectile.h"
-#include "GameSprite.h"
+#include "AppDelegate.h"
 
+//how vertically far apart a block can be from its neighbours, in number of blocks
+//directly proportional to maximum hill slope
+#define Y_OFFSET 3                 
 
-#define Y_OFFSET      3                                      //how vertically far apart a block can be from its neighbours, in number of blocks
-#define MIN_ABS       1                                      //the lowest level a block can be generated at
-#define MAX_ABS       MIN_ABS+5                              //the highest level a block can be generated at. must be MIN_ABS + n
-#define BLOCK_PERCENT 10                                     //number of columns will be generated
-#define BLOCK_Y       int(BLOCK_PERCENT/100*winSize.height)  //vertical size of blocks. must be a divisor of MIN_ABS
-#define BLOCK_X       BLOCK_Y                                //horizontal size of blocks
+//the lowest level a block can be generated at
+#define MIN_ABS 1                           
 
-#define DIRT_FILE     "Resources/res/textures/boden.png"
-#define GRASS_FILE    "Resources/res/textures/oberflaeche_gras.png"
+//the highest level a block can be generated at. must be MIN_ABS + n
+#define MAX_ABS MIN_ABS+5                  
+
+//% of screen width blocks take up. must be an integer divisor of 100
+//inversely proportional to number of columns and to performance
+//directly proportional to terrain resolution and terrain destruction precision
+#define BLOCK_PERCENT 10                           
+
+//horizontal size of blocks
+#define BLOCK_X double(WINDOW_W)*BLOCK_PERCENT/100  
+
+//vertical size of blocks
+#define BLOCK_Y BLOCK_X                     
+
+#define DIRT_FILE   "res/textures/boden.png"
+#define GRASS_FILE  "res/textures/oberflaeche_gras.png"
+
+//square sizes of the .png files
+#define DIRT_SIZE   417.0                       
+#define GRASS_SIZE  100.0
 
 using namespace cocos2d;
-
-const Size winSize = Director::getInstance()->getWinSizeInPixels();
 
 class Ground {
 public:
 
-	//returns a vector containing pointers to all sprites that form the terrain, 
-	//all with physics bodies and rects. They are ordered bottom-up and left-to-right.
-	//call this function only once
-	//to destroy terrain, simply iterate over this and check one by one explosion to block collision
-	static std::vector<Sprite*> createGround();
-
-private:
-	int doSrand;
+	/*Instructions of use:
+	*Call this function only once (!)
+	*If function srand() is called somewhere else, remove it from either this function or the other call place.
+	*Call it from a layer like this: Ground::createGround(*this);
+	*Store the returned cocos2d::Vector<Sprite*> as usual.
+	*Blocks are stored bottom-up and left-to-right.
+	*All blocks are Sprites* with attached PhysicsBody (already containing position and size).
+	*Top blocks have grass texture, the rest have dirt texture.
+	*Modify the first 4 defines of this file to adjust terrain generation parameters.
+	*/
+	static Vector<Sprite*> createGround(Layer& layer);
 };
 
-//int Ground::doSrand = 1;
 
