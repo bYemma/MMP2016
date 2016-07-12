4164ad432fe9c7b9d90388ecf88a4c0abad79076
Patrick <Patrick@WORKHARD>
2016-07-12 12:41:33 +0200

    Tutorial and Story

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -28,6 +28,23 @@ bool MenuScene::init()
 	bg->setPosition(_sSize.width * 0.5, _sSize.height * 0.5);
 	addChild(bg);
 
+	const std::string font = "res/fonts/Minecraft.ttf";
+
+	// Create Labels to show round and game time
+	std::string tutstr = "TUTORIAL:\n\nPress  START  to play.\nHold and release  SPACE  to fire a projectile.\nChange your weapon by pressing  1(Rocket)  2(Nade)  3(Bullet) \nYou can move with  A  and  D  to left and right. Jump with  SHIFT \nAdjust your aim by holding  UP  and  DOWN";
+	auto tutorial = Label::createWithTTF(tutstr, font, 22);
+	tutorial->setPosition(Vec2(_sSize.width*0.30, _sSize.height * 0.90));
+	tutorial->setTextColor(Color4B::WHITE);
+	//_roundtimelabel->setZOrder(5);
+	addChild(tutorial);
+
+	std::string storystr = "HOW IT ALL BEGAN . . .\n\nAll hell broke loose when Blue lost against Red in the EM Semifinale.\n\nThe players grabbed their weapons and started shooting ballz.\n\nThe stadium collapsed and the showdown between red and blue began\n";
+	auto story = Label::createWithTTF(storystr, font, 14);
+	story->setPosition(Vec2(_sSize.width*0.2, _sSize.height * 0.60));
+	story->setTextColor(Color4B::WHITE);
+	//_roundtimelabel->setZOrder(5);
+	addChild(story);
+
 	ui::Button* startButton = ui::Button::create("start_button.png", "", "", ui::TextureResType::PLIST);
 	startButton->setPosition(Vec2(_sSize.width * 0.5 - 143, _sSize.height * 0.5 - 254));
 	startButton->addClickEventListener(CC_CALLBACK_0(MenuScene::start, this));
